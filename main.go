8ad6c1fb7285c25685d6ae0840e91b75934349fe
trieungochai <trieungochai.dev@gmail.com>
2024-09-14 17:14:01 +0700

    define the LoadBalancer and Server

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ type LoadBalancer struct {
 }
 
 // Represents a backend server with a URL and a health status. The mutex ensures that the health status can be updated or checked safely across multiple requests.
-type Servcer struct {
+type Server struct {
 	URL       *url.URL
 	IsHealthy bool
 	Mutex     sync.Mutex
