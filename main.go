021c0ee9d7c1d2de8b5fb591d781310fdd857d41
trieungochai <trieungochai.dev@gmail.com>
2024-09-15 04:46:50 +0700

    add Round Robin algo

--- main.go
+++ main.go
@@ -18,6 +18,27 @@ type Server struct {
 	Mutex     sync.Mutex
 }
 
+func (lb *LoadBalancer) getNextServer(servers []*Server) *Server {
+	lb.Mutex.Lock()
+	defer lb.Mutex.Unlock()
+
+	for i := 0; i < len(servers); i++ {
+		idx := lb.Current % len(servers)
+		nextServer := servers[idx]
+		lb.Current++
+
+		nextServer.Mutex.Lock()
+		isHealthy := nextServer.IsHealthy
+		nextServer.Mutex.Unlock()
+
+		if isHealthy {
+			return nextServer
+		}
+	}
+
+	return nil
+}
+
 func main() {
 
 }
