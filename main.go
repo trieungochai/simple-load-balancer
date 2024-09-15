63d4329177f8ada25df42bc7cc71c897e88a3ee1
trieungochai <trieungochai.dev@gmail.com>
2024-09-15 23:29:41 +0700

    handle requests and start LB

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
@@ -120,5 +121,42 @@ func (lb *LoadBalancer) getNextServer(servers []*Server) *Server {
 }
 
 func main() {
+	config, err := loadConfig("config.json")
+	if err != nil {
+		log.Fatalf("Error loading configuration: %s", err.Error())
+	}
 
+	healthCheckInterval, err := time.ParseDuration(config.HealthCheckInterval)
+	if err != nil {
+		log.Fatalf("Invalid health check interval: %s", err.Error())
+	}
+
+	var servers []*Server
+	for _, serverUrl := range config.Servers {
+		u, _ := url.Parse(serverUrl)
+		server := &Server{URL: u, IsHealthy: true}
+		servers = append(servers, server)
+		go healthChecks(server, healthCheckInterval)
+	}
+
+	lb := LoadBalancer{Current: 0}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		server := lb.getNextServer(servers)
+		if server == nil {
+			http.Error(w, "No healthy server available", http.StatusServiceUnavailable)
+			return
+		}
+
+		// adding this header just for checking from which server the request is being handled.
+		// this is not recommended from security perspective as we don't want to let the client know which server is handling the request.
+		w.Header().Add("X-Forwarded-Server", server.URL.String())
+		server.ReverseProxy().ServeHTTP(w, r)
+	})
+
+	log.Println("Starting load balancer on port", config.Port)
+	err = http.ListenAndServe(config.Port, nil)
+	if err != nil {
+		log.Fatalf("Error starting load balancer: %s\n", err.Error())
+	}
 }
